6649b7fb2fa43797e98ef59f6d0edfbc7f70d216
francescoaltieri <francescoaltieri@hotmail.it>
2025-09-01 11:02:51 +0200

    Backup 2025 09 01
    Backup 2025 09 01

--- CostantiVariabiliPubbliche.vb
+++ CostantiVariabiliPubbliche.vb
@@ -1,3 +1,3 @@
 ﻿Public Module CostantiVariabiliPubbliche
-    Public ReadOnly ConnString As String = "Server=192.168.1.201,1433;Database=DB-GesPu;User Id=sa; Password=Sql2022@Adm2024..;TrustServerCertificate=True;"
+    Public ReadOnly ConnString As String = "Server=10.0.0.10,1433;Database=DB-GesPu;User Id=sa; Password=Sql2022@Adm2024..;TrustServerCertificate=True;"
 End Module
